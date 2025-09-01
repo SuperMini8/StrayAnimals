bf0d93fed34090238e07bf7c2c22018a723d1dbe
小八 <>
2025-09-01 09:28:36 +0800

    創建專案，設定無 Storyboard 起始頁面

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # StrayAnimals
 一個使用政府API查詢台灣流浪動物的App
+
+政府的動物認領養 Open Data URL
+https://data.moa.gov.tw/open_detail.aspx?id=QcbUEzN6E6DL
+
+預計使用：
+1. Swift Concurrency
+2. Coordinator
+3. MVVM
+4. Combine
+5. Code UI
+
+畫面：
+1. List
+2. Information
---
+++ StrayAnimals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrayAnimals
+//
+//  Created by 小八 on 2025/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
