9b5c8acf83af3c57bb34281223146ee31780e558
小八 <>
2025-11-04 15:31:30 +0800

    調整Code 可以 Build 過

--- StrayAnimals/Extensions/UIViewController + Extension.swift
+++ StrayAnimals/Extensions/UIViewController + Extension.swift
@@ -9,19 +9,19 @@ import UIKit
 
 extension UIViewController {
     
-    func checkIfInSigninFlow() -> Bool {
-        if let nav = self as? UINavigationController {
-            if let _ = nav.viewControllers.first as? WelcomeViewController {
-                return true
-            } else if let _ = nav.viewControllers.first as? LoginViewController {
-                return true
-            } else {
-                return false
-            }
-        } else {
-            return false
-        }
-    }
+//    func checkIfInSigninFlow() -> Bool {
+//        if let nav = self as? UINavigationController {
+//            if let _ = nav.viewControllers.first as? WelcomeViewController {
+//                return true
+//            } else if let _ = nav.viewControllers.first as? LoginViewController {
+//                return true
+//            } else {
+//                return false
+//            }
+//        } else {
+//            return false
+//        }
+//    }
     
     func presentFullScreen(_ viewControllerToPresent: UIViewController, animated: Bool = true, completion: (() -> Void)? = nil) {
         if self.presentedViewController == nil { // MARK: - Fix present modally an active controller,
